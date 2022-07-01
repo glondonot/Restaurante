418b9a4051db7cda6a1bafc01f0eecb7ee7b7dc1
Delt404 <79664081+Delt404@users.noreply.github.com>
2022-07-01 09:13:02 -0500

    Update elHash.py

--- elHash.py
+++ elHash.py
@@ -31,31 +31,121 @@ class HashMap:
                                 if pair[0] == key:
                                         return pair[1]
                 return None
+			
+        def print(self):
+                print('---PHONEBOOK----')
+                for item in self.map:
+                        if item is not None:
+                                print(str(item))
 
-Recetas_Hash = HashMap()
-Recetas_Hash.append("1","Hamburguesa")
-Recetas_Hash.append("Hamburguesa","5000")
-Recetas_Hash.append("2","HotDog")
-Recetas_Hash.append("HotDog","5000")
-Recetas_Hash.append("3","Lasaña")
-Recetas_Hash.append("Lasaña","13000")
-Recetas_Hash.append("4","Fritanga")
-Recetas_Hash.append("Fritanga","15000")
-Recetas_Hash.append("5","Ajiaco")
-Recetas_Hash.append("Ajiaco","18000")
-Recetas_Hash.append("6","FelipeGod")
-Recetas_Hash.append("FelipeGod","99999999999999")
+recetas = HashMap()
+#Desayunos
+recetas.append("1","Coliflor al horno con especias")
+recetas.append("Coliflor al horno con especias","5000")
+recetas.append("2","Omelette de huevo")
+recetas.append("Omelette de huevo","5000")
+recetas.append("3","Cocochas de bacalao al pil pil")
+recetas.append("Cocochas de bacalao al pil pil","13000")
+recetas.append("4","Zamburiñas al horno")
+recetas.append("Zamburiñas al horno","15000")
 
-recetas = [
-    ["id", "comida", "precio"],
-    ["1", "Hamburguesa", 5000],
-    ["2", "HotDog", 5000],
-    ["3", "Lasaña", 13000],
-    ["4", "Fritanga", 15000],
-    ["5", "Ajiaco", 18000],
-    ["6", "FelipeGod", 99999999999999],
-]
+#Platos fuertes
+recetas.append("5","Pechugas de pollo al horno")
+recetas.append("Pechugas de pollo al horno","18000")
+recetas.append("6","Tataki de bonito")
+recetas.append("Tataki de bonito","99999999999999")
+#Acompañamientos y ensaladas
+
+#Bebidas
+
+
+
+
+
+
+recetas.append("7","Dorada a la sidra")
+recetas.append("Dorada a la sidra","99999999999999")
+recetas.append("8","Pulpo al horno con patatas")
+recetas.append("Pulpo al horno con patatas","99999999999999")
+recetas.append("9","Lomo de cerdo al ajillo")
+recetas.append("Lomo de cerdo al ajillo","99999999999999")
+recetas.append("10","Solomillo de cerdo con salsa hoisin")
+recetas.append("Solomillo de cerdo con salsa hoisin","99999999999999")
+recetas.append("11","Lenguado a la plancha")
+recetas.append("Lenguado a la plancha","99999999999999")
+recetas.append("12","Bonito a la plancha")
+recetas.append("Bonito a la plancha","99999999999999")
+recetas.append("13","Calamares al ajillo")
+recetas.append("Calamares al ajillo","99999999999999")
+recetas.append("14","Batido de sandía")
+recetas.append("Batido de sandía","99999999999999")
+recetas.append("15","Sepia al horno")
+recetas.append("Sepia al horno","99999999999999")
+recetas.append("16","Bruschetta")
+recetas.append("Bruschetta","99999999999999")
+recetas.append("7","Dorada al horno con limón")
+recetas.append("Dorada al horno con limón","99999999999999")
+recetas.append("7","Puré de zanahoria")
+recetas.append("Puré de zanahoria","99999999999999")
+recetas.append("7","Bacalao rebozado")
+recetas.append("Bacalao rebozado","99999999999999")
+recetas.append("7","Provolone al horno")
+recetas.append("Provolone al horno","99999999999999")
+recetas.append("7","Lubina al horno con limón")
+recetas.append("Lubina al horno con limón","99999999999999")
+recetas.append("7","Tortilla de acelgas")
+recetas.append("Tortilla de acelgas","99999999999999")
+recetas.append("7","Lomo de cerdo empanado (rebozado perfecto)")
+recetas.append("Lomo de cerdo empanado (rebozado perfecto)","99999999999999")
+recetas.append("7","Batido de melón")
+recetas.append("Batido de melón","99999999999999")
+recetas.append("7","Patatas rellenas de jamón y queso al horno")
+recetas.append("Patatas rellenas de jamón y queso al horno","99999999999999")
+recetas.append("7","Salmón en salsa de cebolla")
+recetas.append("Salmón en salsa de cebolla","99999999999999")
+recetas.append("7","Flan de plátano")
+recetas.append("Flan de plátano","99999999999999")
+recetas.append("7","Ensaladilla rusa")
+recetas.append("Ensaladilla rusa","99999999999999")
+recetas.append("7","Batido de fresa y plátano")
+recetas.append("Batido de fresa y plátano","99999999999999")
+recetas.append("7","Casabe")
+recetas.append("Casabe","99999999999999")
+recetas.append("7","Hervido valenciano")
+recetas.append("Hervido valenciano","99999999999999")
+recetas.append("7","Ensalada de atún y huevo con mayonesa")
+recetas.append("Ensalada de atún y huevo con mayonesa","99999999999999")
+recetas.append("7","Carpaccio de ternera con parmesano")
+recetas.append("Carpaccio de ternera con parmesano","99999999999999")
+recetas.append("7","Dorada a la plancha")
+recetas.append("Dorada a la plancha","99999999999999")
+recetas.append("7","Paletilla de cordero al horno asada a las finas hierbas")
+recetas.append("Paletilla de cordero al horno asada a las finas hierbas","99999999999999")
+recetas.append("7","Salmón a la plancha fácil")
+recetas.append("Salmón a la plancha fácil","99999999999999")
+recetas.append("7","Barquitas de endivias")
+recetas.append("Barquitas de endivias","99999999999999")
+recetas.append("7","Frijoles")
+recetas.append("Frijoles","99999999999999")
+recetas.append("7","Fritos de rape o de pixín")
+recetas.append("Fritos de rape o de pixín","99999999999999")
+recetas.append("7","Muslos de pollo al horno")
+recetas.append("Muslos de pollo al horno","99999999999999")
+recetas.append("7","Parmentier de patata")
+recetas.append("Parmentier de patata","99999999999999")
+recetas.append("7","Salmón en papillote al horno con verduras")
+recetas.append("Salmón en papillote al horno con verduras","99999999999999")
+recetas.append("7","Solomillo de cerdo a la pimienta")
+recetas.append("Solomillo de cerdo a la pimienta","99999999999999")
+recetas.append("7","Uvas rellenas de queso")
+recetas.append("Uvas rellenas de queso","99999999999999")
+
+recetas.append("0Hamburguesa","Comidas Rápidas")
+recetas.append("0Hamburguesa","Comidas Rápidas")
 
 a = input("Elija una opcion 1 a 6: ")
-print(Recetas_Hash.get(a)) #Así se saca el str de la comida osea "recetas[a][1]"
-print(int(Recetas_Hash.get(Recetas_Hash.get(a)))) #así se saca el int del precio osea "recetas[a][2]"
+print(recetas.get(str(a))) #Así se saca el str de la comida osea "recetas[a][1]"
+print(int(recetas.get(recetas.get(str(a))))) #así se saca el int del precio osea "recetas[a][2]"
+
+b= "0" + recetas.get(str(a)) #Esto se usaría para saber a que categoria pertenece el plato
+print("El pedido " + recetas.get(str(a)) + " pertenece a la categoria "+ recetas.get(b))
