89a9811e9eb26ff8faa8265b37e6214a5f35a8a6
Delt404 <79664081+Delt404@users.noreply.github.com>
2022-07-01 09:03:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,7 @@
 import tkinter
 from estructuras import Pila
 from estructuras import Queue
-
-                                #!!!!!!!!!!!!!!!!!!!!!!no se si quieran poner esta estructura del hash en otro archivo o si acá nomás :v
-                                #como sea que lo pongan recuerden borrar estos comentarios
-class HashMap:
-        def __init__(self):
-                self.size = 6
-                self.map = [None] * self.size
-		
-        def _get_hash(self, key):
-                hash = 0
-                for char in str(key):
-                        hash += ord(char)
-                return hash % self.size
-		
-        def append(self, key, value):
-                key_hash = self._get_hash(key)
-                key_value = [key, value]
-		
-                if self.map[key_hash] is None:
-                        self.map[key_hash] = list([key_value])
-                        return True
-                else:
-                        for pair in self.map[key_hash]:
-                                if pair[0] == key:
-                                        pair[1] = value
-                                        return True
-                        self.map[key_hash].append(key_value)
-                        return True
-			
-        def get(self, key):
-                key_hash = self._get_hash(key)
-                if self.map[key_hash] is not None:
-                        for pair in self.map[key_hash]:
-                                if pair[0] == key:
-                                        return pair[1]
-                return None
+from estructuras import HashMap
 
 # Añadir elementos al menú, y añadirles un precio correspondiente a cada elemento
 recetas = HashMap()
